a702cdf2d3ec0aba4ca5e517c525ac1a0cb8eae6
Lamberto Basti <basti.lamberto@gmail.com>
2025-01-07 02:02:19 +0100

    Set version dynamically based on GitHub event (#2)
    Updated the `version` property to be determined by the GitHub event context. If triggered by a release, the version is extracted from the tag name; otherwise, it defaults to "1.0-SNAPSHOT". This ensures versioning aligns with release automation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,19 @@ plugins {
 }
 
 group = "com.github.lamba92"
-version = "1.0-SNAPSHOT"
+
+val githubRef =
+    System.getenv("GITHUB_EVENT_NAME")
+        ?.takeIf { it == "release" }
+        ?.let { System.getenv("GITHUB_REF") }
+        ?.removePrefix("refs/tags/")
+        ?.removePrefix("v")
+
+version =
+    when {
+        githubRef != null -> githubRef
+        else -> "1.0-SNAPSHOT"
+    }
 
 kotlin {
     jvmToolchain(8)
